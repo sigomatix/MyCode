abd8dda1d9cf09782c1eb79bb38ae0fed5892095
Sigo Matix <sigomatix@hotmail.com>
2011-10-26 01:40:37 +0100

    switch to jint

--- V8Test/JSTestGenerator.cs
+++ V8Test/JSTestGenerator.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Noesis.Javascript;
 using System.IO;
+using Jint;
 
 namespace V8Test
 {
+    public class TestFile
+    {
+        public string FilePath { get; set; }
+        public List<string> TestMethods { get; set; }
+
+        public TestFile(string path)
+        {
+            FilePath = path;
+        }
+    }
+
 
     public class JSTestGenerator
     {
-        private List<string> testFiles = new List<string>();
+        private List<TestFile> testFiles = new List<TestFile>();
         private List<string> includeFiles = new List<string>();
         private string relocationPath;
+        private static object locker = new Object();
 
         public JSTestGenerator()
         {
@@ -25,46 +37,50 @@ namespace V8Test
 
         public void AddTestFile(string p)
         {
-            testFiles.Add(p);
+            var testFile = new TestFile(p);
+            testFiles.Add(testFile);
+
+            var context = new JintEngine();
+            {
+                context.Run(@"
+  function square(x) { 
+    return x * x;
+  }");
+                var methodsList = (string)context.Run(
+@"b ='test';
+                for(var m in this)
+                {
+                    if(m.toString().toLowerCase().indexOf('test')>=0)
+                    {
+                        b+=m+';'
+                    }
+b+=m.toString();
+                };
+                return b;");
+                testFile.TestMethods = methodsList.Trim(';').Split(';').ToList();
+            }
+
         }
 
         public IList<string> GetAllTestMethods()
         {
-            using (var context = new JavascriptContext())
-            {
-                foreach (var testFile in testFiles)
-                {
-                    context.Run(File.ReadAllText(testFile));
-                }
-
-                var methodsList = (string)context.Run(
-@"var b ='';
-for(var m in this)
-{
-    if(m.toString().toLowerCase().indexOf('test')>=0)
-    {
-        b+=m+';'
-    }
-};
-b;");
-                return methodsList.Trim(';').Split(';').ToList();
-            }
+            return testFiles.SelectMany(file => file.TestMethods).ToList();
         }
 
         public string GenerateTestCodeForMethod(string p)
         {
             var builder = new StringBuilder();
-            builder.Append("using(var context = new Noesis.Javascript.JavascriptContext())\r\n{\r\n");
+            builder.Append("var context = new Jint.JintEngine();\r\n{\r\n");
             foreach (var include in includeFiles) builder.Append(string.Format("    context.Run(File.ReadAllText(@\"{0}\"));\r\n", include));
-            foreach (var test in testFiles)
+            var test = testFiles.FirstOrDefault(t => t.TestMethods.Contains(p));
+            string finalPath = test.FilePath;
+            if (!string.IsNullOrEmpty(relocationPath))
             {
-                string finalPath = test;
-                if (!string.IsNullOrEmpty(relocationPath))
-                {
-                    finalPath = Path.Combine(relocationPath, Path.GetFileName(test));
-                }
-                builder.Append(string.Format("    context.Run(File.ReadAllText(@\"{0}\"));\r\n", finalPath));
+                finalPath = Path.Combine(relocationPath, Path.GetFileName(test.FilePath));
             }
+            builder.Append(string.Format("    context.Run(@\"__FILENAME__ = '{0}';\");\r\n", finalPath));
+            builder.Append(string.Format("    context.Run(File.ReadAllText(@\"{0}\"));\r\n", finalPath));
+
             builder.Append(string.Format("    context.Run(\"{0}();\");\r\n", p));
             builder.Append("}");
             return builder.ToString();
--- V8Test/JSTestGeneratorTests.cs
+++ V8Test/JSTestGeneratorTests.cs
@@ -3,7 +3,6 @@ using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Noesis.Javascript;
 using System.IO;
 
 namespace V8Test
@@ -16,14 +15,13 @@ namespace V8Test
     {
        
         [TestMethod]
-        [DeploymentItem(@"tests\AdditionTests.js")]
-        [DeploymentItem(@"tests\DivideTests.js")]
         public void ShouldListProperlyAllTestMethods()
         {
             var jsTestGenerator = new JSTestGenerator();
 
-            jsTestGenerator.AddTestFile("AdditionTests.js");
-            jsTestGenerator.AddTestFile("DivideTests.js");
+            jsTestGenerator.AddTestFile(@"tests\AdditionTests.js");
+            jsTestGenerator.AddTestFile(@"tests\DivideTests.js");
+            jsTestGenerator.SetTestFilesRelocationPath("tests");
 
             var testMethods = jsTestGenerator.GetAllTestMethods();
 
@@ -36,24 +34,60 @@ namespace V8Test
         }
 
         [TestMethod]
-        [DeploymentItem(@"libs\Assert.js")]
-        [DeploymentItem(@"tests\AdditionTests.js")]
-        [DeploymentItem(@"tests\DivideTests.js")]
+        public void ShouldListProperlyAllTestMethods2()
+        {
+            var jsTestGenerator = new JSTestGenerator();
+
+            jsTestGenerator.AddTestFile(@"tests\AdditionTests.js");
+            jsTestGenerator.AddTestFile(@"tests\DivideTests.js");
+            jsTestGenerator.SetTestFilesRelocationPath("tests");
+
+            var testMethods = jsTestGenerator.GetAllTestMethods();
+
+            Assert.AreEqual("TestAddditionShouldAddPositives", testMethods[0]);
+            Assert.AreEqual("TestAddditionShouldAddNegatives", testMethods[1]);
+            Assert.AreEqual("TestAddditionShouldAddNegativesAndPositiv", testMethods[2]);
+            Assert.AreEqual("TestDivideBySameShouldReturnOne", testMethods[3]);
+            Assert.AreEqual("TestDivideByZeroShouldReturnInfinity", testMethods[4]);
+            Assert.AreEqual(5, testMethods.Count);
+        }
+
+        [TestMethod]
+        public void ShouldListProperlyAllTestMethods3()
+        {
+            var jsTestGenerator = new JSTestGenerator();
+
+            jsTestGenerator.AddTestFile(@"tests\AdditionTests.js");
+            jsTestGenerator.AddTestFile(@"tests\DivideTests.js");
+            jsTestGenerator.SetTestFilesRelocationPath("tests");
+
+            var testMethods = jsTestGenerator.GetAllTestMethods();
+
+            Assert.AreEqual("TestAddditionShouldAddPositives", testMethods[0]);
+            Assert.AreEqual("TestAddditionShouldAddNegatives", testMethods[1]);
+            Assert.AreEqual("TestAddditionShouldAddNegativesAndPositiv", testMethods[2]);
+            Assert.AreEqual("TestDivideBySameShouldReturnOne", testMethods[3]);
+            Assert.AreEqual("TestDivideByZeroShouldReturnInfinity", testMethods[4]);
+            Assert.AreEqual(5, testMethods.Count);
+        }
+
+        [TestMethod]
         public void ShouldGenerateCodeWhenNoIncludesProvided()
         {
             var jsTestGenerator = new JSTestGenerator();
 
-            jsTestGenerator.AddIncludeFile("Assert.js");
-            jsTestGenerator.AddTestFile("AdditionTests.js");
-            jsTestGenerator.AddTestFile("DivideTests.js");
+            jsTestGenerator.AddIncludeFile(@"libs\Assert.js");
+            jsTestGenerator.AddTestFile(@"tests\AdditionTests.js");
+            jsTestGenerator.AddTestFile(@"tests\DivideTests.js");
+            jsTestGenerator.SetTestFilesRelocationPath("tests");
 
             /* TODO optimize to run only the test code that includes the test method */
             var expectedCode =
-@"using(var context = new Noesis.Javascript.JavascriptContext())
+@"var context = new Noesis.Javascript.JavascriptContext();
 {
-    context.Run(File.ReadAllText(@""Assert.js""));
-    context.Run(File.ReadAllText(@""AdditionTests.js""));
-    context.Run(File.ReadAllText(@""DivideTests.js""));
+    context.Run(File.ReadAllText(@""libs\Assert.js""));
+    context.Run(@""__FILENAME__ = 'tests\AdditionTests.js';"");
+    context.Run(File.ReadAllText(@""tests\AdditionTests.js""));
     context.Run(""TestAddditionShouldAddPositives();"");
 }";
 
