667a5b82bb5716ad499de488fc666a4ead2a87b3
unknown <sigomatix@sigomatix-PC.(none)>
2011-11-21 00:03:34 +0000

    missing file

---
+++ PMSTest/PMSTest/TestRunnerStub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PMSTest
+{
+    public class TestRunnerStub:ITestRunner
+    {
+        public IList<string> ExecutedMethods { get; private set; }
+
+        public object Run(IMethodInfo[] methodInfo)
+        {
+            ExecutedMethods = methodInfo.Select(m => m.Name).ToList();
+            return null;
+        }
+    }
+}
